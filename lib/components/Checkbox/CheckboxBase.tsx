546a8869013923043a3a105a9cd3b0ec2254f546
David <davidaasm@gmail.com>
2024-07-15 01:01:49 +0800

    update

--- lib/components/Checkbox/CheckboxBase.tsx
+++ lib/components/Checkbox/CheckboxBase.tsx
@@ -21,7 +21,7 @@ export const CheckboxBase: React.FC<CheckboxBaseProps> = ({
         className={twMerge(
           "relative z-10",
           "peer block h-full w-full cursor-pointer appearance-none rounded border-2 border-gray-400 focus:outline-4 focus:outline-blue-500",
-          "checked:border-blue-500",
+          "checked:border-blue-600",
           isError && "border-red-500",
           className,
         )}
@@ -30,7 +30,7 @@ export const CheckboxBase: React.FC<CheckboxBaseProps> = ({
         }}
         {...props}
       />
-      <span className="absolute left-1/2 top-1/2 hidden h-[150%] w-[150%] -translate-x-1/2 -translate-y-1/2 transform items-center justify-center overflow-hidden rounded font-bold text-blue-500 peer-checked:flex">
+      <span className="absolute left-1/2 top-1/2 hidden h-[150%] w-[150%] -translate-x-1/2 -translate-y-1/2 transform items-center justify-center overflow-hidden rounded font-bold text-blue-600 peer-checked:flex">
         <AiFillCheckSquare className="h-full w-full rounded" />
       </span>
     </div>
